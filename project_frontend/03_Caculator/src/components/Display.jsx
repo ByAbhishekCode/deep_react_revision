7685c0c511d551e08eab8099b82fce3acf119908
Abhishek <abhikumar1260@gmail.com>
2025-06-27 12:52:34 +0530

    fix

--- project_frontend/03_Caculator/src/components/Display.jsx
+++ project_frontend/03_Caculator/src/components/Display.jsx
@@ -1,7 +1,12 @@
 import styles from "./Display.module.css";
 const Display = ({ displayValue }) => {
   return (
-    <input className={styles.display} type="text" value={displayValue} readOnly></input>
+    <input
+      className={styles.display}
+      type="text"
+      value={displayValue}
+      readOnly
+    ></input>
   );
 };
 
